438a5ca0cbb5ba5659b62a103f72505e9b65ff16
Marco Moncada <99331256+MarkupMonkey@users.noreply.github.com>
2022-08-14 14:41:23 +0200

    Booklist

---
+++ src/books.js
@@ -0,0 +1,17 @@
+export const data = [
+    {
+        id: 1, img: "https://images-eu.ssl-images-amazon.com/images/I/71tJ6aYPdDL._AC_UL604_SR604,400_.jpg",
+        title: 'Il caso Alaska Sanders',
+        author: 'Joel Dicker'
+    },
+    {
+        id: 2, img: "https://images-eu.ssl-images-amazon.com/images/I/612rIA6UjeL._AC_UL604_SR604,400_.jpg",
+        title: 'Riscrivi le pagine della Tua Vita',
+        author: 'Anna De Simone'
+    },
+    {
+        id: 3, img: "https://images-eu.ssl-images-amazon.com/images/I/810hiyIndYL._AC_UL604_SR604,400_.jpg",
+        title: 'fabbricante di lacrime',
+        author: 'Erin Doom'
+    }
+]
